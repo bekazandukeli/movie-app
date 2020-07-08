b0b4eb9746057ba4c1058c5a9751d363ee8b45f9
bekazandukeli <bekazandukeli@gmail.com>
2020-07-08 15:02:30 +0400

    Updates

---
+++ precache-manifest.6061921e5f9277b2e7744ef55619e5aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e085938f17f0362f174846d78f060fa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6234867d6247b24ada2",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bb0f12bdbd905453cc0d",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b6234867d6247b24ada2",
+    "url": "/static/js/2.526dfcb7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.526dfcb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb0f12bdbd905453cc0d",
+    "url": "/static/js/main.b06b8394.chunk.js"
+  },
+  {
+    "revision": "466c1a7fdaa58576fa1f",
+    "url": "/static/js/runtime-main.7d4b7ba4.js"
+  }
+]);
